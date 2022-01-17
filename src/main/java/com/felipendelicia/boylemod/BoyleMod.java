48df33bd44a3551ac0f0a9e7ba86b4f24413dd63
1felipo <delicia4581@gmail.com>
2022-01-16 21:21:54 -0300

    init config minecraft mod

--- src/main/java/com/felipendelicia/boylemod/BoyleMod.java
+++ src/main/java/com/felipendelicia/boylemod/BoyleMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.felipendelicia.boylemod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,13 +19,15 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod(BoyleMod.MOD_ID)
+public class BoyleMod
 {
+    public static final String MOD_ID = "boylemod";
+
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
+    public BoyleMod() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
@@ -48,7 +50,6 @@ public class ExampleMod
 
     private void doClientStuff(final FMLClientSetupEvent event) {
         // do something that can only be done on the client
-        LOGGER.info("Got game settings {}", event.getMinecraftSupplier().get().options);
     }
 
     private void enqueueIMC(final InterModEnqueueEvent event)
